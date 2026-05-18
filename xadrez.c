1c8e43f9f997330299ace3eb2c821def258ba060
Bruno Trindade <270747136+brunomtrindade-sudo@users.noreply.github.com>
2026-05-18 17:33:17 -0300

    Adicionado 'desafio intermediário'
    Adicionado movimentos do cavalo usando loops alinhados

--- xadrez.c
+++ xadrez.c
@@ -15,7 +15,7 @@ int main() {
         printf("A torre se moveu 1 casa para a direita\n");
         
     }
-        printf("A torre moveu 5 casas para a direita!\n");
+    printf("A torre moveu 5 casas para a direita!\n");
 
     //Movimentos Bispo:
     printf("\nMovimento do Bispo:\n");
@@ -24,7 +24,7 @@ int main() {
         printf("O bispo se moveu 1 casa para Cima Direita\n");
         i++;
     }
-        printf("O bispo moveu 5 casas para a diagonal superior direita!\n");
+    printf("O bispo moveu 5 casas para a diagonal superior direita!\n");
 
     // Movimento da Rainha:
     printf("\nMovimento da Rainha:\n");
@@ -33,18 +33,23 @@ int main() {
         printf("A rainha se moveu 1 casa para a esquerda\n");
         i++;
     } while(i < rainha);
-        printf("A Rainha moveu 8 casas para a esquerda!\n");
+    printf("A Rainha moveu 8 casas para a esquerda!\n");
+
+    //Movimentos Cavalo:
+    printf("\nMovimento do Cavalo:\n");
+
+    for(i = 0; i < 2; i++) {
+        printf("O cavalo se moveu 1 casa para baixo\n");
+
+        int j = 0;
+
+        while(j < 1) {
+            printf("O cavalo se moveu 1 casa para a esquerda\n");
+            j++;
+        }
+    }
+
+    printf("O cavalo realizou o movimento em L!\n");
 
     return 0;
 }
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
